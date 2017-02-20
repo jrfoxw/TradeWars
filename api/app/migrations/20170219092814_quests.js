9ed3aa39d67e9d263c04872763ab6c50a0e350af
jrfoxw <jrfoxw@gmail.com>
2017-02-20 08:55:33 -0600

    Added items,npcs,creatures migrates. Updated quests migrate

--- api/app/migrations/20170219092814_quests.js
+++ api/app/migrations/20170219092814_quests.js
@@ -2,18 +2,18 @@
 exports.up = function(knex, Promise) {
    return knex.schema.createTableIfNotExists('quests', function(table){
       table.increments('id');
-      table.string('user_id');
       table.string('quest_id');
       table.string('quest_title');
       table.string('quest_type');
       table.string('quest_level');
       table.string('quest_difficulty');
       table.string('quest_desc');
-
+      table.string('quest_stages_total'); // how many stages in quest
+      table.string('quest_stages'); // needs at least one stage.
    });
   
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('quests')
 };
--- api/app/migrations/20170220082205_items.js
+++ api/app/migrations/20170220082205_items.js
@@ -1,8 +1,39 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTableIfNotExists('items', function(table){
+        table.increments('id');
+
+        // Basic stats
+        table.string('item_id');
+        table.string('item_name');
+        table.string('item_type'); // Weapon,Armor,Potion,Ring,Amulet,etc
+        table.string('item_element'); // Nature,Fire,Wind,Water,Air,Dark,Light
+        table.string('item_level');
+        table.string('item_class_specific');
+        table.string('item_desc');
+        table.string('item_weight');
+        table.string('item_goes_ivn_slot');
+
+        // Combat stats
+        table.string('item_attack_damage');
+        table.string('item_defense_level');
+        table.string('item_magic_damage');
+        table.string('item_chance_of_spell_fizzle');
+
+        // Booleans
+        table.boolean('item_is_magical');
+        table.boolean('item_does_protect');
+        table.boolean('item_does_damage');
+        table.boolean('item_does_heal');
+
+
+
+    });
+
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('items')
 };
+
+
